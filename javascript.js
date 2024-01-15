108e6ccdef4f1f1170e2ed289e8160a4a9debb65
Rita Marques <50811505+ritammarques@users.noreply.github.com>
2024-01-15 22:10:30 +0000

    commit princicpal

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function plugin(){
+    console.log("click plugin");
+    document.getElementById("plugin_tab").classList.add("active");
+    document.getElementById("game_tab").classList.remove("active");
+    document.getElementById("plugin").classList.add("active");
+    document.getElementById("plugin").classList.remove("not-active")
+    document.getElementById("game").classList.add("not-active");
+    document.getElementById("game").classList.remove("active");
+}
+function game(){
+    console.log("click game");
+    document.getElementById("game_tab").classList.add("active");
+    document.getElementById("plugin_tab").classList.remove("active");
+    document.getElementById("game").classList.add("active");
+    document.getElementById("game").classList.remove("not-active");
+    document.getElementById("plugin").classList.add("not-active");
+    document.getElementById("plugin").classList.remove("active");
+}
+
+document.getElementById("game_tab").addEventListener("click", game);
+document.getElementById("plugin_tab").addEventListener("click", plugin);
